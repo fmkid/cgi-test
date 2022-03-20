bb4a6af7a065772ed27325f61bb55a117f56807f
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2022-03-20 01:05:10 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import cgi
+# Headers
+print("Content-Type: text/plain")
+print()
+print("""<html>
+<head>
+    <title>Título de la página</title>
+</head>
+<h3>Hola CGI!</h3>
+</html>""")
