b3d8414bd7c12aa7d0a5aeb502f77f3507322c87
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2026-08-11 16:40:28 -0500

    Update proxy.py

--- scripts/proxy.py
+++ scripts/proxy.py
@@ -80,6 +80,7 @@ for proxy_info in proxy_list:
             response = requests.get(TARGET_URL, timeout=8, verify=False, headers=headers)
             response.raise_for_status()
             us_data = response.json()
+            print(us_data[0], json_data[0], us_data[0] == json_data[0])
 
             if us_data[0] == json_data[0]:
                 print(f"Invalid data: list for {country.upper()} is the same than US. Trying next proxy...")
