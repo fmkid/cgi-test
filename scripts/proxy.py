15b5318ea84f80c047ab3bc39a106d91961c1564
Mao Moreno <86756714+fmkid@users.noreply.github.com>
2026-08-13 17:12:57 -0500

    Update proxy.py

--- scripts/proxy.py
+++ scripts/proxy.py
@@ -28,7 +28,7 @@ def fetch_url_list(proxies=None, headers=None):
     return [
         {
             "_id": item["_id"],
-            "name": item["name"].removeprefix("OO: ").removeprefix("Pluto TV "),
+            "name": item["name"].removeprefix("OO:").removeprefix("Pluto TV").strip(),
             "ep_id": gen_ep_id(
                 item["timelines"][0]["episode"]["name"],
                 item["timelines"][0]["episode"]["number"],
